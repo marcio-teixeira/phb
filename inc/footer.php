085f881fb9fa9e5fbee46db61ec3a03c31eadff6
marcio-teixeira <marcio.c73@gmail.com>
2017-07-17 14:28:57 -0300

    fim da home

--- inc/footer.php
+++ inc/footer.php
@@ -1,11 +1,47 @@
 <footer>
-	<div class="container">
-		<div class="fim">
-			<span class="fl">Todos os direitos reservados - <?php echo date('Y') ?></span>
-			<a class="fr" target="_blank" title="criação de websites - RS DEZOITO" href="http://www.rsdezoito.com.br/" rel="nofollow">RS DEZOITO</a>
-		</div>
-	</div>
-	
+    <div class="boxFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <img class="logoFooter" title="PHB Solar" alt="PHB Solar" src="<?php echo url_path() ?>images/logo-phb-solar.png">
+                    <h4>PHB SOLAR</h4>
+                    <p>Trabalhe com qualidade, segurança e suporte técnico. Seja um de nossos parceiros.</p>
+                    <img class="imgInmetro" alt="Logo Inmetro" title="Inmetro" src="<?php echo url_path() ?>images/img-inmetro.png">
+                </div>
+                <div class="col-sm-4">
+                    <h4>PHB SOLAR</h4>
+                    <p>
+                        R. Aroaba, 129 - Vila Leopoldina - São Paulo - SP<br>
+                        Cep: 05315-020
+                    </p>
+                    <div class="mapa">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2586.5789056844196!2d-46.732003377260305!3d-23.533402699484103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef8a2ccdad9af%3A0x7b5d6aac80944b63!2sR.+Aroaba%2C+129+-+Vila+Leopoldina%2C+S%C3%A3o+Paulo+-+SP%2C+05315-020!5e0!3m2!1spt-BR!2sbr!4v1500310713035" width="100%" height="130" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <nav class="menuFooter">
+                        <ul>            	
+                            <li><a href="<?php echo url_path() ?>" title="Institucional">Institucional</a></li>
+                            <li><a href="<?php echo url_path() ?>" title="Produtos">Produtos</a></li>
+                            <li><a href="<?php echo url_path() ?>" title="Assistência Técnica no Brasil">Assistência Técnica no Brasil</a></li>
+                            <li><a href="<?php echo url_path() ?>" title="Treinamentos">Treinamentos</a></li>
+                            <li><a href="<?php echo url_path() ?>" title="Forma de Pagamento">Forma de Pagamento</a></li>
+                            <li><a href="<?php echo url_path() ?>" title="Monitoramento">Monitoramento</a></li>
+                            <li><a href="<?php echo url_path() ?>" title="Tutoriais">Tutoriais</a></li>
+                            <li><a href="<?php echo url_path() ?>" title="Contato">Contato</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fim">
+        <div class="container">
+        <span class="fl"><?php echo date('Y') ?> PHB Solar®. Todos os direitos reservados.</span>
+        <a class="fr" target="_blank" title="criação de websites - RS DEZOITO" href="http://www.rsdezoito.com.br/" rel="nofollow">RS DEZOITO</a>
+        </div>
+    </div>
+
 </footer>
 <script src="<?php echo url_path() ?>js/jquery-migrate-1.2.1.min.js"></script>
 <script src="<?php echo url_path() ?>js/jquery-1.11.3.min.js"></script>
@@ -16,8 +52,8 @@
 
 
 <script type="text/javascript">
-	$(window).load(function() {
-		$("#menuMobile").mmenu({
+    $(window).load(function() {
+        $("#menuMobile").mmenu({
             offCanvas: {
                 position: "right"
             },
@@ -29,11 +65,11 @@
                 title	: "MENU"
             }
         });    
-		$.reject({  
-			display: ['firefox','chrome','opera','msie'] // Displays only firefox, chrome, opera and IE     
-		});  
-	  
-		return false;  
-	});
+        $.reject({  
+            display: ['firefox','chrome','opera','msie'] // Displays only firefox, chrome, opera and IE     
+        });  
+
+        return false;  
+    });
 </script>
-		
+
--- index.php
+++ index.php
@@ -185,21 +185,21 @@ require('inc/globals.php'); // Require in all pages
                     <article>
                         <div class="container">
                             <div class="row">
-                                <div class="col-sm-4">
+                                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                                     <div class="ultimasBlog">
                                         <h4 class="title">ÚLTIMAS DO <strong>BLOG</strong></h4>
                                         <a class="imgLink" href="#" title="Título do Post">
                                             <img class="img-responsive" alt="Título do Post" src="<?php echo url_path() ?>images/img-blog.jpg">
                                             <span class="hover"></span>    
                                         </a>
-                                        <a href="#" title="Título do Post">
+                                        <a class="linkTexto" href="#" title="Título do Post">
                                             <strong>Lorem ipsum</strong>
                                             Vivamus vel nibh feugiat, venenatis erat ullamcorper, rutrum eros.
                                         </a>
                                     </div>
                                 </div>
 
-                                <div class="col-sm-5 col-xs-6">
+                                <div class="col-lg-4 col-md-4 col-sm-8 col-xs-7">
                                     <div class="noticiasHome">
                                         <h4 class="title">NOTÍCIAS</h4>
                                         <div class="noticia">
@@ -227,8 +227,10 @@ require('inc/globals.php'); // Require in all pages
                                     </div>
                                 </div>
 
-                                <div class="col-sm-3 col-xs-6">
-                                    <div class="fb-page" data-href="https://www.facebook.com/PHBSOLAR01/" data-tabs="timeline" data-height="280" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/PHBSOLAR01/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/PHBSOLAR01/">PHB Solar</a></blockquote></div>
+                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-5">
+                                    <div class="boxFacebook">
+                                        <div class="fb-page" data-href="https://www.facebook.com/PHBSOLAR01/" data-tabs="timeline" data-height="280" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/PHBSOLAR01/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/PHBSOLAR01/">PHB Solar</a></blockquote></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
